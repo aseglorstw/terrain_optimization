dd07a020439c4161d2ec8ac5b8c88d972befe1d8
Robert <robert.bakhtigariev@gmail.com>
2024-08-08 15:57:55 +0200

    update: good weights.

--- terrain_optimizer.py
+++ terrain_optimizer.py
@@ -2,8 +2,9 @@ import argparse
 import os
 import sys
 import torch
-from pytorch3d.loss import point_mesh_face_distance
 from pytorch3d.loss import (
+    point_mesh_face_distance,
+    point_mesh_edge_distance,
     mesh_laplacian_smoothing,
     mesh_normal_consistency,
     mesh_edge_loss
@@ -12,6 +13,7 @@ from tqdm.auto import tqdm
 import device_processer, heigh_map_processer, point_cloud_processer, visualizer
 import mesh_processer
 from pytorch3d.structures import Pointclouds
+
 
 def check_input(path_to_mesh):
     if not os.path.isfile(path_to_mesh):
@@ -23,20 +25,21 @@ def optimization_process(wheels_point_cloud, roof_point_cloud, init_terrain_mesh
     deform_vertices = torch.full(init_terrain_mesh.verts_packed().shape, 0.0, device=device, requires_grad=True)
     optimizer = torch.optim.SGD([deform_vertices], lr=1.0, momentum=0.9)
     terrain_mesh = None
-    for i in tqdm(range(1000), ncols=80, ascii=True, desc='Optimization'):
+    for i in tqdm(range(5000), ncols=80, ascii=True, desc='Optimization'):
         optimizer.zero_grad()
         terrain_mesh = init_terrain_mesh.offset_verts(deform_vertices)
-        loss_distance_wheels = point_mesh_face_distance(terrain_mesh, wheels_point_cloud)
-        loss_distance_roof = point_mesh_face_distance(terrain_mesh, roof_point_cloud)
+        loss_face_distance_wheels = point_mesh_face_distance(terrain_mesh, wheels_point_cloud)
+        loss_face_distance_roof = point_mesh_face_distance(terrain_mesh, roof_point_cloud)
+        loss_edge_distance_wheels = point_mesh_edge_distance(terrain_mesh, wheels_point_cloud)
+        loss_edge_distance_roof = point_mesh_edge_distance(terrain_mesh, roof_point_cloud)
         loss_laplacian = mesh_laplacian_smoothing(terrain_mesh, method="uniform")
         loss_normal = mesh_normal_consistency(terrain_mesh)
         loss_edge = mesh_edge_loss(terrain_mesh)
-        loss = 0.05 * loss_distance_wheels + loss_laplacian + loss_edge + 0.5 * loss_normal
+        loss = 0.5 * (loss_face_distance_wheels + loss_edge_distance_wheels) + 0.5 * loss_laplacian + 1.5 * loss_edge + loss_normal - 0.001 * (loss_face_distance_roof + loss_edge_distance_roof)
         loss.backward()
         optimizer.step()
-    visualizer.visualize_mesh_open3d(terrain_mesh)
-    visualizer.visualize_mesh_open3d_with_points(terrain_mesh, point_cloud_processer.combine_point_clouds([wheels_point_cloud, roof_point_cloud]))
-
+        if i % 50 == 0 and i >= 200:
+            visualizer.visualize_mesh_open3d_with_points(terrain_mesh, point_cloud_processer.combine_point_clouds([wheels_point_cloud, roof_point_cloud]))
 
 
 def main(arguments):
