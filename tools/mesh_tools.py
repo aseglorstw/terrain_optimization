7a6b5d79ee3e608b7f3fb10cebda11ab59e68901
Robert <robert.bakhtigariev@gmail.com>
2024-08-16 10:12:31 +0200

    update: first experiment is added.

--- tools/mesh_tools.py
+++ tools/mesh_tools.py
@@ -1,4 +1,5 @@
 import torch
+import pyvista as pv
 from pytorch3d.io import load_obj, save_obj
 from pytorch3d.structures import Meshes
 from tools import device_tools
@@ -11,6 +12,16 @@ def load_mesh(path_to_obj_file, device):
     return Meshes(verts=[vertices], faces=[faces])
 
 
+def move_mesh_to_VRAM(vertices, faces, device):
+    return vertices.to(device), faces.verts_idx.to(device)
+
+
+def simplify_mesh_and_save(path_to_mesh, output_path):
+    mesh = pv.read(path_to_mesh)
+    simplified_mesh = mesh.decimate_pro(0.75)
+    simplified_mesh.save(output_path)
+
+
 def save_mesh(mesh, path_to_file):
     vertices = mesh.verts_packed().detach().cpu().numpy()
     faces = mesh.faces_packed().detach().cpu().numpy()
@@ -18,10 +29,3 @@ def save_mesh(mesh, path_to_file):
                           faces=[torch.tensor(faces, dtype=torch.int64)])
     save_obj(path_to_file, mesh_to_save.verts_packed(), mesh_to_save.faces_packed())
 
-
-def move_mesh_to_VRAM(vertices, faces, device):
-    return vertices.to(device), faces.verts_idx.to(device)
-
-
-def normalize_mesh(vertices):
-    return (vertices - vertices.mean(0)) / max(vertices.abs().max(0)[0])
--- touch_point_determinant.py
+++ touch_point_determinant.py
@@ -1,20 +1,24 @@
 import meshlib.mrmeshpy as mr
-import os
+from tools import mesh_tools
 import time
 
 
-def determine_touch_points_meshlib():
-    mesh = mr.loadMesh("/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/transformed_robot.obj")
-    terrain_mesh = mr.loadMesh("/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/terrain_mesh.obj")
+def determine_touch_points_meshlib(path_to_robot_mesh, terrain_mesh, output_path):
+    robot_mesh = mr.loadMesh(path_to_robot_mesh)
+    terrain_mesh = mr.loadMesh(terrain_mesh)
     start = time.time()
-    intersection = mr.boolean(terrain_mesh, mesh, mr.BooleanOperation.Intersection)
+    intersection = mr.boolean(terrain_mesh, robot_mesh, mr.BooleanOperation.Intersection)
     end = time.time()
     print(end - start)
-    mr.saveMesh(intersection.mesh, "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/meshlib/intersection.stl")
+    mr.saveMesh(intersection.mesh, output_path)
 
 
 def main():
-    determine_touch_points_meshlib()
+    path_to_robot_mesh = "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/first_experiment/transformed_robot.stl"
+    path_to_terrain_mesh = "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/terrain_mesh.obj"
+    output_path = "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/second_experiment/transformed_robot_simplified.stl"
+    # determine_touch_points_meshlib()
+    mesh_tools.simplify_mesh_and_save(path_to_robot_mesh, output_path)
 
 
 if __name__ == '__main__':
