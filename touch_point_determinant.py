6a66f9e2eea6291d53f3a1af349a42830a29247d
Robert <robert.bakhtigariev@gmail.com>
2024-08-16 09:51:44 +0200

    update: robot is located on center of mesh.

--- touch_point_determinant.py
+++ touch_point_determinant.py
@@ -1,32 +1,21 @@
-import argparse
-import torch
 import meshlib.mrmeshpy as mr
-from tools import (
-    device_tools,
-    height_map_tools,
-    point_cloud_tools,
-    visualize_tools,
-    mesh_tools,
-    math_tools
-)
+import os
 import time
 
 
-def determine_touch_points():
-    mesh = mr.loadMesh("experiments/transformed_robot.obj")
-    terrain_mesh = mr.loadMesh("experiments/terrain_mesh.obj")
+def determine_touch_points_meshlib():
+    mesh = mr.loadMesh("/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/transformed_robot.obj")
+    terrain_mesh = mr.loadMesh("/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/terrain_mesh.obj")
     start = time.time()
-    diff = mr.boolean(terrain_mesh, mesh, mr.BooleanOperation.Intersection)
+    intersection = mr.boolean(terrain_mesh, mesh, mr.BooleanOperation.Intersection)
     end = time.time()
     print(end - start)
-    mr.saveMesh(diff.mesh, mr.Path("intersection.obj"))
+    mr.saveMesh(intersection.mesh, "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/meshlib/intersection.obj")
 
 
-def main(arguments):
-    determine_touch_points()
+def main():
+    determine_touch_points_meshlib()
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--mesh_path", default='', type=str, help="Path to the .obj file")
-    main(parser.parse_args())
+    main()
