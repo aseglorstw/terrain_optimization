cdfe63239cb74e14f46cda72611edb7b99df07bd
Robert <robert.bakhtigariev@gmail.com>
2024-08-16 10:42:43 +0200

    update: second experiment is added.

--- touch_point_determinant.py
+++ touch_point_determinant.py
@@ -1,6 +1,8 @@
 import meshlib.mrmeshpy as mr
 from tools import mesh_tools
 from tools import visualize_tools
+from tools import device_tools
+from tools import math_tools
 import time
 
 
@@ -15,10 +17,20 @@ def determine_touch_points_meshlib(path_to_robot_mesh, terrain_mesh, output_path
 
 
 def main():
-    path_to_robot_mesh = "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/second_experiment/transformed_robot_simplified.stl"
-    path_to_terrain_mesh = "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/second_experiment/terrain_mesh.stl"
-    output_path = "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/second_experiment/meshlib/intersection.stl"
-    determine_touch_points_meshlib(path_to_robot_mesh, path_to_terrain_mesh, output_path)
+    # path_to_robot_mesh = "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/second_experiment/transformed_robot_simplified.stl"
+    path_to_terrain_mesh = "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/second_experiment/terrain_mesh.obj"
+    # output_path = "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/second_experiment/meshlib/intersection.stl"
+    # determine_touch_points_meshlib(path_to_robot_mesh, path_to_terrain_mesh, output_path)
+    # device = device_tools.choose_device()
+    # robot_mesh = mesh_tools.load_mesh("/home/robert/catkin_ws/src/URDF2mesh/meshes_extracted/husky.obj", device)
+    # terrain_mesh = mesh_tools.load_mesh(path_to_terrain_mesh, device)
+    # transform_matrix = math_tools.get_transformation_matrix([0, 0, 0], [0, 0, 0.05])
+    # transformed_robot = math_tools.transform_robot_model(robot_mesh, transform_matrix, terrain_mesh)
+    # mesh_tools.save_mesh(transformed_robot, "/home/robert/catkin_ws/src/robot_touch_point_detection/transformed_robot_2.obj")
+    mesh_tools.simplify_mesh_and_save("/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/second_experiment/transformed_robot_2.obj", "/home/robert/catkin_ws/src/robot_touch_point_detection/experiments/second_experiment/transformed_robot_2_simplified.stl", 0.9)
+
+
+
 
 
 if __name__ == '__main__':
